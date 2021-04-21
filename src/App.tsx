089f28b25a2a379ecc0e40acc37299a8fec7c7af
Muhammad Arslan Sajid <masajid390@gmail.com>
2021-04-22 00:52:02 +0200

    removed unnecessary boilerplate code create by create-react-app

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,8 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
 
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    </ThemeProvider>
-  );
+  return <ThemeProvider theme={theme}></ThemeProvider>;
 }
 
 export default App;
