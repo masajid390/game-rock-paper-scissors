4ffba1d727cecf139cdcbba3b0456e81005c8e90
Muhammad Arslan Sajid <masajid390@gmail.com>
2021-04-22 00:48:15 +0200

    added styled-components with theme.

---
+++ src/theme/index.ts
@@ -0,0 +1,12 @@
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  borderRadius: "8px",
+
+  colors: {
+    main: "hsl(0, 0%, 100%)",
+    secondary: "hsl(229, 25%, 31%)",
+  },
+};
+
+export { theme };
