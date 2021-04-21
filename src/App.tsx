e74d76e7974ba4c38cc3dc9752617e9926f63f34
Muhammad Arslan Sajid <masajid390@gmail.com>
2021-04-22 01:17:56 +0200

    setup the app/game container.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
-import { ThemeProvider } from "styled-components";
+import { FC } from "react";
+import styled, { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
 
-function App() {
-  return <ThemeProvider theme={theme}></ThemeProvider>;
-}
+const AppContainer = styled.div`
+  background-image: radial-gradient(hsl(214, 47%, 23%), hsl(237, 49%, 15%));
+  height: 100vh;
+  width: 100vw;
+  max-width: 1366px;
+  margin: 0 auto;
+`;
+
+const App: FC = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <AppContainer></AppContainer>
+    </ThemeProvider>
+  );
+};
 
 export default App;
