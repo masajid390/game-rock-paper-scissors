063b0048e6356beb8e7fbf4739cc6fc966de624b
Muhammad Arslan Sajid <masajid390@gmail.com>
2021-04-24 01:09:48 +0200

    Added IconButton component.

--- src/components/lib/Button.tsx
+++ src/components/lib/Button.tsx
@@ -1,3 +1,4 @@
+import { FC } from "react";
 import styled from "styled-components";
 const Button = styled.button`
   color: ${({ theme }) => `${theme.colors.main}`};
@@ -11,4 +12,49 @@ const Button = styled.button`
   cursor: pointer;
 `;
 
-export { Button };
+interface IconButtonContainerProps {
+  size: number;
+  gradientFromColor: string;
+  gradientToColor: string;
+  border: number;
+}
+const IconButtonContainer = styled("div")<IconButtonContainerProps>`
+  ${({ size, gradientFromColor, gradientToColor, border }) => {
+    const shadow = border / 2.5;
+    return `
+      width: ${size}px;
+      height: ${size}px;
+      align-items: center;
+      justify-content: center;
+      display: flex;
+      border-radius: 50%;
+      border: ${border}px solid ${gradientToColor};
+      border-bottom: ${border - shadow}px solid ${gradientToColor};
+      background: white;
+      box-shadow: 0 ${shadow}px ${gradientFromColor}, inset 0 ${shadow}px lightgrey;
+      cursor: pointer;
+      user-select: none;
+  `;
+  }}
+`;
+
+export interface IconButtonProps extends IconButtonContainerProps {
+  iconSrc: string;
+}
+const IconButton: FC<IconButtonProps> = ({
+  size,
+  gradientFromColor,
+  gradientToColor,
+  iconSrc,
+  border,
+}) => (
+  <IconButtonContainer
+    size={size}
+    gradientFromColor={gradientFromColor}
+    gradientToColor={gradientToColor}
+    border={border}
+  >
+    <img src={iconSrc} />
+  </IconButtonContainer>
+);
+export { Button, IconButton };
