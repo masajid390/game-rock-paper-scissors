5685c218b29f0acecccdc72a476ff3c6297dd250
Muhammad Arslan Sajid <masajid390@gmail.com>
2021-04-23 23:46:15 +0200

    Added Triangle, Paper, Rock, and Scissor Icons.

---
+++ src/components/lib/icons/index.tsx
@@ -0,0 +1,6 @@
+import RockIcon from "./RockIcon.svg";
+import PaperIcon from "./PaperIcon.svg";
+import ScissorIcon from "./ScissorIcon.svg";
+import BGTriangle from "./BGTriangle.svg";
+
+export { RockIcon, PaperIcon, ScissorIcon, BGTriangle };
