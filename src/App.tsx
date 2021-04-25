e46dc242600d8f856379ea111ef07142de54d08a
Muhammad Arslan Sajid <masajid390@gmail.com>
2021-04-25 18:11:19 +0200

    Added game to application, and improved App container layout.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,24 @@
 import { FC } from "react";
 import styled, { ThemeProvider } from "styled-components";
 import { theme } from "./theme";
+import { Game } from "./components/game";
 
 const AppContainer = styled.div`
-  background-image: radial-gradient(hsl(214, 47%, 23%), hsl(237, 49%, 15%));
+  background-image: radial-gradient(
+    circle,
+    hsl(214, 47%, 23%),
+    hsl(237, 49%, 15%)
+  );
   height: 100vh;
   width: 100vw;
-  max-width: 1366px;
-  margin: 0 auto;
 `;
 
 const App: FC = () => {
   return (
     <ThemeProvider theme={theme}>
-      <AppContainer></AppContainer>
+      <AppContainer>
+        <Game />
+      </AppContainer>
     </ThemeProvider>
   );
 };
