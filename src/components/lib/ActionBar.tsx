18f82e7a956d65ab62dfe0845321123f80f3ea54
Muhammad Arslan Sajid <masajid390@gmail.com>
2021-04-28 01:06:00 +0200

    improved ActionBar component to support modes button as well.

--- src/components/lib/ActionBar.tsx
+++ src/components/lib/ActionBar.tsx
@@ -1,14 +1,10 @@
 import styled from "styled-components";
-import { down } from "styled-breakpoints";
 
 const ActionBar = styled.div`
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: flex-end;
-  ${down("xs")} {
-    justify-content: center;
-  }
+  justify-content: space-between;
 `;
 
 export { ActionBar };
